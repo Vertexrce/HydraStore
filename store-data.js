4096cac0c43402f37cf4e34baf4792f46cf76835
Vertex <chandlerpridgen55@gmail.com>
2026-06-14 22:45:06 -0500

    Create store-data.js

---
+++ store-data.js
@@ -0,0 +1,19 @@
+const storeItems = [
+{
+    name: "VIP",
+    price: "$9.99",
+    features: [
+        "Priority Queue",
+        "VIP Chat Tag",
+        "Starter Kit",
+        "Discord Role"
+    ]
+},
+{
+    name: "Wood Pack",
+    price: "$2.99",
+    features: [
+        "100K Wood"
+    ]
+}
+];
